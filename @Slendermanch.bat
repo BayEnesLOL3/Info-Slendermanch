33c825673ed3bfd73f548436cb11765501407c2a
@Slendermanch <95964864+BayEnesLOL3@users.noreply.github.com>
2022-03-31 14:55:13 +0300

    Add files via upload

---
+++ @Slendermanch.bat
@@ -0,0 +1,51 @@
+@echo off
+
+color a
+
+title @Slendermanch.sys
+
+echo.
+echo This software is information for @Slendermanch.
+echo.
+echo This program is in working now.
+echo.
+echo This program is dangeros for ur .sys files!
+echo. 
+echo I just kidding but dont install any virus on ur comupter!
+echo.
+echo Creator - @Slendermanch 
+echo.
+echo A.sys 
+echo.
+echo Please install @Slendermanch.sys form my github for optimize pc.
+echo.
+echo The @Slendermanch.sys optimize ur comuputer no virus or any dangerous software.
+echo.
+:menu
+echo ======================================
+echo 1 Start (press 1 to start app)
+echo 2 Exit (press 2 for exit "software")
+echo ======================================
+
+set /p lul=
+if %lul%== 1 goto 1
+if %lul%== 2 goto 2
+
+:1
+title Info 
+
+echo Now working!.
+
+pause
+
+goto menu 
+
+:2
+
+exit 
+
+exit /F
+
+exit 999
+
+exit
